723bae3df1b4c828167e30f2cb4f291a1b21af48
Simon Buist <simon.buist@gmail.com>
2014-02-03 23:18:39 +0000

    Add the unruly beast that is intersectall-cc

--- functions.scm
+++ functions.scm
@@ -253,7 +253,7 @@
 
 ; Chapter 13: Hop, Skip, and Jump
 
-(define intersect-letrec
+(define intersect
   (lambda (set1 set2)
     (letrec
       ((I (lambda (set)
@@ -265,3 +265,36 @@
 	      (else
 		(I (cdr set)))))))
       (I set1))))
+
+(define intersectall
+  (lambda (lset)
+    (letrec
+      ((A (lambda (lset)
+	    (cond
+	      ((null? (cdr lset))
+	       (car lset))
+	      (else (intersect (car lset)
+			       (A (cdr lset))))))))
+      (cond
+	((null? lset) (quote ()))
+	(else (A lset))))))
+
+
+; I dunno... This looks horrible. If we're doing sanity checks why not just check if there's () in lset FIRST, along with the outer (cond)?
+(define intersectall-cc
+  (lambda (lset)
+    (call-with-current-continuation
+      (lambda (hop)
+	(letrec
+	  ((A (lambda (lset)
+		(cond
+		  ((null? (car lset))
+		   (hop (quote ())))
+		  ((null? (cdr lset))
+		   (car lset))
+		  (else
+		    (intersect (car lset)
+			       (A (cdr lset))))))))
+	  (cond
+	    ((null? lset) (quote ()))
+	    (else (A lset))))))))
